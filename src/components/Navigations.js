7ca0b00dd3c61140772ff20751b724d2423d692d
zel-hub7 <zelalemyohannes62@gmail.com>
2023-08-29 03:58:51 -0700

    add some styles

--- src/components/Navigations.js
+++ src/components/Navigations.js
@@ -1,24 +1,28 @@
 import React from "react";
-import './navigation.css';
+import "./navigation.css";
 import { NavLink } from "react-router-dom";
 const Navigations = () => {
   return (
     <nav className="navigation">
       <div>
         <h1>Logo</h1>
-        </div>
+      </div>
       <ul className="nav-lists">
         <li className="nav-link">
-          <NavLink exact='true' to="/Rockets" activeclassname="active">Rockets</NavLink>
+          <NavLink exact="true" to="/Rockets" activeclassname="active">
+            Rockets
+          </NavLink>
         </li>
         <li>
-          <NavLink exact='true' to="/Missions" activeclassname="active">Missions</NavLink>
+          <NavLink exact="true" to="/Missions" activeclassname="active">
+            Missions
+          </NavLink>
         </li>
+        <li>|</li>
         <li>
-            |
-        </li>
-        <li>
-          <NavLink exact='true' to="/Profile" activeclassname="active">My Profile</NavLink>
+          <NavLink exact="true" to="/Profile" activeclassname="active">
+            My Profile
+          </NavLink>
         </li>
       </ul>
     </nav>
