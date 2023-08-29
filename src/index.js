422a235c15d05234dbbe9bb0692cbc27a5612e96
Mohamed Roshdy Elkhadragy <xmedox20111@gmail.com>
2023-08-29 19:39:22 +0300

    Revert "Added the initial files for the global store"

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store'; 
 import App from './App';
 import Navigations from './components/Navigations';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <BrowserRouter>
       <Navigations />
       <App />
     </BrowserRouter>
-  </Provider>  
+  </React.StrictMode>,
 );
