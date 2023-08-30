b695c008d66315a6a7c43530e95cdc1b199116e1
zel-hub7 <zelalemyohannes62@gmail.com>
2023-08-30 02:39:34 -0700

    fetch data

--- src/Redux/Dragons/DragonsSlice.js
+++ src/Redux/Dragons/DragonsSlice.js
@@ -1,5 +1,4 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const url = "https://api.spacexdata.com/v4/dragons";
@@ -7,6 +6,7 @@ const initialState = {
   isLoading: false,
   data: [],
   error: "",
+  joinedDragons: [],
 };
 const getData = (data) =>
   data.map((dragons) => ({
@@ -15,6 +15,7 @@ const getData = (data) =>
     description: dragons.description,
     image: dragons.flickr_images,
     reserved: false,
+   
   }));
 export const fetchDragonsData = createAsyncThunk(
   "dragons/fetchDragons",
@@ -28,3 +29,61 @@ export const fetchDragonsData = createAsyncThunk(
     }
   }
 );
+const dragonSlice = createSlice({
+  name: "dragons",
+  initialState,
+  reducers: {
+    reserveDragon: (state, action) => {
+      const newState = state.data.map((dragon) => {
+        if (dragon.id === action.payload) {
+          return { ...dragon, reserved: true };
+        }
+        return dragon;
+      });
+      return {
+        ...state,
+        data: newState,
+        joinedDragons: [...state.joinedDragons, action.payload],
+      };
+    },
+    cancelReserveDragon: (state, action) => {
+      const newState = state.data.map((dragon) => {
+        if (dragon.id === action.payload) {
+          return { ...dragon, reserved: false };
+        }
+        return dragon;
+      });
+      return {
+        ...state,
+        data: newState,
+        joinedDragons: state.joinedDragons.filter(
+          (dragonId) => dragonId !== action.payload
+        ),
+      };
+    },
+  },
+  
+  extraReducers: (builder) => {
+    builder
+        .addCase(fetchDragonsData.pending, (state) => ({
+            ...state,
+            isLoading: true,
+        }))
+        .addCase(fetchDragonsData.fulfilled, (state, action) => ({
+            ...state,
+            isLoading: false,
+            data: action.payload,
+
+        }))
+        .addCase(fetchDragonsData.rejected, (state,action) => ({
+            ...state,
+            isLoading: false,
+            error: action.error.message,
+        }));
+  }
+
+
+});
+
+export const { reserveDragon, cancelReserveDragon } = dragonSlice.actions;
+export default dragonSlice.reducer;
