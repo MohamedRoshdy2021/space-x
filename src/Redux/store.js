a44e0c5864ce3b871bc26c29495c638cb6f16eb5
Mohammed roshdy <xmedox20111@gmail.com>
2023-08-30 16:11:50 +0300

    Added the Rockets component functionality

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import rocketsReducer from './Rockets/RocketsSlice';
 
 const store = configureStore({
   reducer: {
-
+    rockets: rocketsReducer,
   },
 });
 
