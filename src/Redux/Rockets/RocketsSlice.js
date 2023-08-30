5fd8fe89b11dec509e7448121f7bde86718e346d
Mohammed roshdy <xmedox20111@gmail.com>
2023-08-30 23:19:11 +0300

    Added the functionlity for the profile rockets

--- src/Redux/Rockets/RocketsSlice.js
+++ src/Redux/Rockets/RocketsSlice.js
@@ -1,21 +1,76 @@
+// import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+// import axios from 'axios';
+
+// export const fetchRockets = createAsyncThunk('rockets/fetchRockets', async () => {
+//   const response = await axios.get('https://api.spacexdata.com/v3/rockets');
+//   return response.data;
+// });
+
+// const rocketsSlice = createSlice({
+//   name: 'Rockets',
+//   initialState: [],
+//   reducers: {},
+//   extraReducers: (builder) => {
+//     builder.addCase(fetchRockets.fulfilled, (state, action) => {
+//       return action.payload
+//     })
+//   },
+// })
+
+// export const selectRockets = (state) => state.rockets;
+
+// export default rocketsSlice.reducer;
+
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-// Async thunk to fetch data from the API
-export const fetchRockets = createAsyncThunk('rockets/fetchRockets', async () => {
-  const response = await axios.get('https://api.spacexdata.com/v3/rockets');
-  return response.data;
+const url = 'https://api.spacexdata.com/v3/rockets'; // Update the URL to the rockets API
+const initialState = {
+  isLoading: false,
+  data: [],
+  reservedRockets: [],
+  error: '',
+};
+
+export const fetchRocketsData = createAsyncThunk('rockets/fetchRockets', async () => {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
 });
 
-const rocketsSlice = createSlice({
-  name: 'Rockets',
-  initialState: [],
-  reducers: {},
+export const rocketsSlice = createSlice({
+  name: 'rockets',
+  initialState,
+  reducers: {
+    reserveRocket: (state, action) => {
+      const rocketId = action.payload;
+      if (!state.reservedRockets.includes(rocketId)) {
+        state.reservedRockets.push(rocketId);
+      }
+    },
+    cancelReservation: (state, action) => {
+      const rocketId = action.payload;
+      state.reservedRockets = state.reservedRockets.filter((id) => id !== rocketId);
+    },
+  },
   extraReducers: (builder) => {
-    builder.addCase(fetchRockets.fulfilled, (state, action) => [...state, ...action.payload]);
+    builder
+      .addCase(fetchRocketsData.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchRocketsData.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.data = action.payload;
+      })
+      .addCase(fetchRocketsData.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
   },
 });
 
-export const selectRockets = (state) => state.rockets;
-
+export const { reserveRocket, cancelReservation } = rocketsSlice.actions;
 export default rocketsSlice.reducer;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,6 +5,14 @@ import './profile.css';
 function Profile() {
   const { data, joinedDragons } = useSelector((store) => store.dragons);
   const reservedDragons = data.filter((dragon) => joinedDragons.includes(dragon.id));
+
+  const reservedRockets = useSelector((state) => state.rockets.reservedRockets);
+  const rockets = useSelector((state) => state.rockets.data);
+
+  const reservedRocketNames = reservedRockets.map((rocketId) => {
+    const rocket = rockets.find((rocket) => rocket.id === rocketId);
+    return rocket ? rocket.rocket_name : '';
+  });
   return (
     <div className="profile">
       <table className="profile-dragons">
@@ -33,18 +41,14 @@ function Profile() {
         </thead>
       </table>
 
-      <table className="profile-dragons">
-        <thead>
-          <tr>
-            <th><h2>My Rockets</h2></th>
-          </tr>
-          <tbody>
-            place holder for my rockets
-
-          </tbody>
-        </thead>
-      </table>
-
+      <div className="profile-dragons">
+        <div className="Rockets-profile-container">
+          <h2>My Rockets:</h2>
+          {reservedRocketNames.map((rocketName) => (
+            <p key={rocketName.id}>{rocketName}</p>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
