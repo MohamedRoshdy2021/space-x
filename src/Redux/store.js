221c0dea302cb67a9bde6b4b322350a620bb3423
Mohammed roshdy <xmedox20111@gmail.com>
2023-08-30 16:21:20 +0300

    Solved conflicts

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rocketsReducer from './Rockets/RocketsSlice';
+import dragonsReducer from './Dragons/DragonsSlice';
 
 const store = configureStore({
   reducer: {
+    dragons: dragonsReducer,
     rockets: rocketsReducer,
   },
 });
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,50 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import './profile.css';
+
 function Profile() {
+  const {data, joinedDragons} = useSelector((store) => store.dragons)
+  const reservedDragons = data.filter((dragon) => joinedDragons.includes(dragon.id))
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className='profile'>
+    <table className='profile-dragons'>
+      <thead>
+        <tr>
+          <th><h2>My Dragons</h2></th>
+        </tr>
+        <tbody className='dragon-body'>
+        {reservedDragons.map((dragon) => (
+            <tr key={dragon.id}>
+              <td>{dragon.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </thead>
+    </table>
+
+    <table className='profile-dragons'>
+      <thead>
+        <tr>
+          <th><h2>My Missions</h2></th>
+        </tr>
+        <tbody>
+            Place holder for my missons
+        </tbody>
+      </thead>
+    </table>
+
+    <table className='profile-dragons'>
+      <thead>
+        <tr>
+          <th><h2>My Rockets</h2></th>
+        </tr>
+        <tbody>
+          place holder for my rockets
+       
+        </tbody>
+      </thead>
+    </table>
+    
     </div>
   );
 }
