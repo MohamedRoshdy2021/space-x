fa1e61bcea3c946244dba1ecab514a208fb6d2c7
zel-hub7 <zelalemyohannes62@gmail.com>
2023-08-31 12:57:03 -0700

    update files

--- src/components/Mission/mission.js
+++ src/components/Mission/mission.js
@@ -1,35 +1,99 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getMissions } from '../../Redux/missions/missionsSlice';
-import MissionComponent from './MissionComponent';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  fetchMissions,
+  joinMission,
+  leaveMission,
+} from '../../Redux/missions/missionsSlice';
 import './mission.css';
-function Mission() {
+
+function Missions() {
+  const missionData = useSelector((store) => store.missions);
+  const {
+    isLoading, missions, error, joinedMissions,
+  } = missionData;
   const dispatch = useDispatch();
-  const { missions} = useSelector((store) => store.missions);
-  console.log(missions);
+
+  function handleJoinMission(id) {
+    dispatch(joinMission(id));
+  }
+
+  function handleLeaveMission(id) {
+    dispatch(leaveMission(id));
+  }
+
   useEffect(() => {
- dispatch(getMissions());
-  }, []);
-  return (
-    <section className="super-mission">
-      <div className="header-container">
-        <p>Mission</p>
-        <p className="hola">Description</p>
-        <p className="status">Status</p>
-        <p className="join">Join</p>
+    dispatch(fetchMissions());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        Error:
+        {error}
       </div>
-      <ul className="mission-container>">
-        { missions.map((mission) => (
-          <MissionComponent
-            key={mission.mission_id}
-            name={mission.mission_name}
-            description={mission.description}
-            id={mission.mission_id}
-            status={mission.reserved}
-          />
-        ))}
-      </ul>
-    </section>
+    );
+  }
+
+  return (
+    <>
+      <table className="missions">
+        <thead>
+          <tr>
+            <th className="mission">Mission</th>
+            <th className="description">Description</th>
+            <th className="status">Status</th>
+            <th className="button">&nbsp;</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.map((mission) => (
+            <tr
+              key={mission.id}
+              className={
+                joinedMissions.includes(mission.id) ? 'rows' : 'join-rows'
+              }
+            >
+              <td className="td-mission">{mission.name}</td>
+              <td className="description td-description">
+                {mission.description}
+              </td>
+              <td className="status">
+                {joinedMissions.includes(mission.id) ? (
+                  <span className="mission-status-leave">Active Member</span>
+                ) : (
+                  <span className="mission-status-join">NOT A MEMBER</span>
+                )}
+              </td>
+              <td className="button">
+                {joinedMissions.includes(mission.id) ? (
+                  <button
+                    type="button"
+                    className="td-leave-mission"
+                    onClick={() => handleLeaveMission(mission.id)}
+                  >
+                    Leave Mission
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    className="td-join-mission"
+                    onClick={() => handleJoinMission(mission.id)}
+                  >
+                    Join Mission
+                  </button>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
-export default Mission;
+
+export default Missions;
