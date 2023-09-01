51c2a614a7893b87d8335003d5ac652b06170109
zel-hub7 <zelalemyohannes62@gmail.com>
2023-09-01 02:14:21 -0700

    test dragonsSlice.js

---
+++ src/components/test/DragonsSlice.test.js
@@ -0,0 +1,66 @@
+import { configureStore } from '@reduxjs/toolkit';
+import dragonsReducer,{fetchDragonsData,reserveDragon,cancelReserveDragon} from '../../Redux/Dragons/DragonsSlice';
+
+describe('dragonsSlice', () => {
+    let store;
+  
+    beforeEach(async () => {
+      store = configureStore({
+        reducer: {
+          dragons: dragonsReducer,
+        },
+      });
+  
+      await store.dispatch(fetchDragonsData());
+    });
+  
+    it('should handle fetching rockets data successfully', () => {
+      const state = store.getState().dragons;
+      expect(state.isLoading).toBe(false);
+      expect(state.error).toBe('');
+      expect(state.data.length).toBeGreaterThan(0);
+    });
+  
+    it('should handle fetching rockets data failure', async () => {
+      const invalidUrl = 'https://api.spacexdata.com/v4/invalid-url';
+      try {
+        await store.dispatch(fetchDragonsData(invalidUrl));
+      } catch (error) {
+        const state = store.getState().dragonss;
+        expect(state.isLoading).toBe(false);
+        expect(state.error).toBe('Network Error');
+      }
+    });
+  
+    it('should reserve a rocket', () => {
+        const stateBefore = store.getState().dragons;
+        const dragonIdToReserve = stateBefore.data[0].id;
+        store.dispatch(reserveDragon(dragonIdToReserve));
+      
+        const stateAfter = store.getState().dragons;
+      
+        const reservedDragon = stateAfter.data.find(
+          (dragon) => dragon.id === dragonIdToReserve,
+        );
+      
+        expect(reservedDragon.reserved).toBe(true);
+        expect(stateAfter.joinedDragons).toContain(dragonIdToReserve);
+      });
+      
+  
+    it('should cancel reservation of a rocket', () => {
+        const stateBefore = store.getState().dragons;
+        const dragonIdToCancel = stateBefore.data[0].id;
+      
+        store.dispatch(cancelReserveDragon(dragonIdToCancel));
+      
+        const stateAfter = store.getState().dragons;
+        const canceledDragon = stateAfter.data.find(
+          (dragon) => dragon.id === dragonIdToCancel,
+        );
+      
+        expect(canceledDragon.reserved).toBe(false);
+        expect(stateAfter.joinedDragons).not.toContain(dragonIdToCancel);
+      });
+      
+  });
